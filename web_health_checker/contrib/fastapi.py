aaf6cd9aad7901bb6a597634ff8a0aacec325bf3
Leo Spratt <enchant97@users.noreply.github.com>
2022-07-20 21:20:02 +0100

    fix for fastapi router

--- web_health_checker/contrib/fastapi.py
+++ web_health_checker/contrib/fastapi.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
 
 router = APIRouter()
 
 
 @router.get(
     "/is-healthy",
-    description="web app health checking")
+    description="web app health checking",
+    response_class=PlainTextResponse)
 async def health_check():
     return "🆗"
